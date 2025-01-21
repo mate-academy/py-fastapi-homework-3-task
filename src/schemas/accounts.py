b3f32f8c023b2a390a9513a560593a084fbe7e7f
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2025-01-21 21:34:13 +0200

    Solution

--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -2,4 +2,57 @@ from pydantic import BaseModel, EmailStr, field_validator
 
 from database import accounts_validators
 
-# Write your code here
+
+class UserBaseModel(BaseModel):
+    email: EmailStr
+
+    @field_validator("email")
+    @classmethod
+    def validate_email(cls, value):
+        return accounts_validators.validate_email(value)
+
+
+class UserRegistrationRequestSchema(UserBaseModel):
+    password: str
+
+    @field_validator("password")
+    @classmethod
+    def validate_email(cls, value):
+        return accounts_validators.validate_password_strength(value)
+
+
+class UserRegistrationResponseSchema(BaseModel):
+    id: int
+    email: EmailStr
+
+    model_config = {"from_attributes": True}
+
+
+class UserActivationRequestSchema(UserBaseModel):
+    token: str
+
+
+class PasswordResetRequestSchema(UserBaseModel):
+    pass
+
+
+class PasswordResetCompleteRequestSchema(UserRegistrationRequestSchema):
+    token: str
+
+
+class UserLoginRequestSchema(UserRegistrationRequestSchema):
+    pass
+
+
+class UserLoginResponseSchema(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str
+
+
+class TokenRefreshRequestSchema(BaseModel):
+    refresh_token: str
+
+
+class TokenRefreshResponseSchema(BaseModel):
+    access_token: str
