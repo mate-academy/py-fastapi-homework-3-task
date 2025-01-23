184d8c281fde778c598a4419f34b6d35d7de2ff0
Mike Shmygel <m.shmygel@gmail.com>
2025-01-23 18:33:42 +0200

    Solution

--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -1,5 +1,76 @@
-from pydantic import BaseModel, EmailStr, field_validator
-
+from pydantic import BaseModel, EmailStr, field_validator, ConfigDict
 from database import accounts_validators
 
-# Write your code here
+#     UserRegistrationResponseSchema,
+#     UserActivationRequestSchema,
+#     MessageResponseSchema,
+#     PasswordResetRequestSchema,
+#     PasswordResetCompleteRequestSchema,
+#     UserLoginResponseSchema,
+#     UserLoginRequestSchema,
+#     TokenRefreshRequestSchema,
+#     TokenRefreshResponseSchema
+
+
+class BaseEmailPasswordSchema(BaseModel):
+    email: EmailStr
+    password: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+    @field_validator("email")
+    @classmethod
+    def validate_email(cls, value):
+        return value.lower()
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, value):
+        return accounts_validators.validate_password_strength(value)
+
+
+class UserRegistrationResponseSchema(BaseModel):
+    id: int
+    email: EmailStr
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class UserRegistrationRequestSchema(BaseEmailPasswordSchema):
+    pass
+
+
+class PasswordResetRequestSchema(BaseModel):
+    email: EmailStr
+
+
+class PasswordResetCompleteRequestSchema(BaseEmailPasswordSchema):
+    token: str
+
+
+class UserLoginRequestSchema(BaseEmailPasswordSchema):
+    pass
+
+
+class UserLoginResponseSchema(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
+
+
+class UserActivationRequestSchema(BaseModel):
+    email: EmailStr
+    token: str
+
+
+class MessageResponseSchema(BaseModel):
+    message: str
+
+
+class TokenRefreshRequestSchema(BaseModel):
+    refresh_token: str
+
+
+class TokenRefreshResponseSchema(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
