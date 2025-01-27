c0fec2c4065dc639ac920ccc7f665f9f1104678c
Vadym Sulim <ohnooq1@gmail.com>
2025-01-27 06:13:51 +0200

    solution

--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -2,4 +2,83 @@ from pydantic import BaseModel, EmailStr, field_validator
 
 from database import accounts_validators
 
-# Write your code here
+
+class BaseSchema(BaseModel):
+    email: EmailStr
+    password: str
+
+    model_config = {"from_attributes": True}
+
+    @field_validator("email")
+    def validate_email(cls, value):
+        return accounts_validators.validate_email(value)
+
+    @field_validator("password")
+    def validate_password(cls, value):
+        return accounts_validators.validate_password_strength(value)
+
+
+class ActivationTokenSchema(BaseSchema):
+    email: EmailStr
+    password: str
+    token: str
+
+
+class UserRegistrationRequestSchema(BaseSchema):
+    pass
+
+
+class UserRegistrationResponseSchema(BaseSchema):
+    id: int
+    email: EmailStr
+
+
+class UserActivationRequestSchema(BaseModel):
+    email: EmailStr
+    token: str
+
+    model_config = {"from_attributes": True}
+
+
+class PasswordResetRequestSchema(BaseModel):
+    email: EmailStr
+
+    @field_validator("email")
+    def validate_email(cls, value):
+        return accounts_validators.validate_email(value)
+
+
+class PasswordResetResponseSchema(BaseSchema):
+    email: EmailStr
+    reset_token: str
+    password: str
+
+
+class PasswordResetCompleteRequestSchema(BaseSchema):
+    email: EmailStr
+    token: str
+    password: str
+
+
+class MessageResponseSchema(BaseModel):
+    message: str
+
+    model_config = {"from_attributes": True}
+
+
+class UserLoginRequestSchema(BaseSchema):
+    pass
+
+
+class UserLoginResponseSchema(BaseModel):
+    token: str
+    refresh_token: str
+    access_token: str
+
+
+class TokenRefreshRequestSchema(BaseModel):
+    refresh_token: str
+
+
+class TokenRefreshResponseSchema(BaseModel):
+    access_token: str
