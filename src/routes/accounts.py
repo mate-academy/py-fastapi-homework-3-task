d375a4e19be58ad70adf9ba7ce3b2f485dd683fd
Den_k0 <denys.kotovskiy@gmail.com>
2025-01-27 20:50:25 +0200

    Implement User Registration

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -18,6 +18,44 @@ from database import (
 from exceptions import BaseSecurityError
 from security.interfaces import JWTAuthManagerInterface
 
+from schemas.accounts import (
+    UserRegistrationResponseSchema,
+    UserRegistrationRequestSchema
+)
+
 router = APIRouter()
 
-# Write your code here
+
+@router.post(
+    "/register/",
+    response_model=UserRegistrationResponseSchema,
+    status_code=201
+)
+def register_user(user_data: UserRegistrationRequestSchema, db: Session = Depends(get_db)):
+    existing_user = db.query(UserModel).filter_by(email=user_data.email).first()
+    if existing_user:
+        raise HTTPException(
+            status_code=409,
+            detail=f"A user with this email {user_data.email} already exists."
+        )
+
+    user_group = db.query(UserGroupModel).filter_by(name=UserGroupEnum.USER).first()
+    new_user = UserModel(
+        email=user_data.email,
+        password=user_data.password,
+        group=user_group
+    )
+    activation_token = ActivationTokenModel(user=new_user)
+
+    try:
+        db.add(new_user)
+        db.add(activation_token)
+        db.commit()
+    except SQLAlchemyError:
+        db.rollback()
+        raise HTTPException(status_code=500, detail="An error occurred during user creation.")
+
+    return UserRegistrationResponseSchema(
+        id=new_user.id,
+        email=new_user.email
+    )
--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -2,4 +2,22 @@ from pydantic import BaseModel, EmailStr, field_validator
 
 from database import accounts_validators
 
-# Write your code here
+
+class UserRegistrationRequestSchema(BaseModel):
+    email: EmailStr
+    password: str
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, password):
+        return accounts_validators.validate_password_strength(password)
+
+    @field_validator("email")
+    @classmethod
+    def validate_email(cls, user_email):
+        return accounts_validators.validate_email(user_email)
+
+
+class UserRegistrationResponseSchema(BaseModel):
+    id: int
+    email: EmailStr
