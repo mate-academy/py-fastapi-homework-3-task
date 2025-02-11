3ddeff3226973b425c12ca56ba38b557125051f3
Valentin Pozniak <waliker448@gmail.com>
2025-02-11 15:47:40 +0200

    solution

--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -1,5 +1,60 @@
+from typing import Optional
 from pydantic import BaseModel, EmailStr, field_validator
 
 from database import accounts_validators
+from database.validators.accounts import validate_password_strength
 
-# Write your code here
+
+class UserBase(BaseModel):
+    email: EmailStr
+
+
+class UserRegistrationResponseSchema(UserBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+
+class UserRegistrationRequestSchema(UserBase):
+    password: str
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, v):
+        return validate_password_strength(v)
+
+
+class UserActivationRequestSchema(UserBase):
+    token: str
+
+
+class PasswordResetRequestSchema(BaseModel):
+    email: EmailStr
+
+
+class PasswordResetCompleteRequestSchema(PasswordResetRequestSchema):
+    token: str
+    password: str
+
+
+class MessageResponseSchema(BaseModel):
+    message: str
+
+
+class UserLoginRequestSchema(UserRegistrationRequestSchema):
+    pass
+
+
+class UserLoginResponseSchema(BaseModel):
+    access_token: Optional[str] = None
+    refresh_token: Optional[str] = None
+    token_type: str = "Bearer"
+
+
+class TokenRefreshRequestSchema(BaseModel):
+    refresh_token: str
+
+
+class TokenRefreshResponseSchema(BaseModel):
+    access_token: str
